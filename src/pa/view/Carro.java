25b338f0fc9f9ccd79d8760ef3106bfe69098f56
Francisco Gonçalves <160244026+fgonca@users.noreply.github.com>
2025-05-16 00:30:59 +0100

    Create Carro.java

---
+++ src/pa/view/Carro.java
@@ -0,0 +1,18 @@
+package pa.view;
+
+public class Carro 
+{
+	public static String[] partes()
+	{
+		String[] carro = new String[6];
+		carro[0] = "              ------------";
+		carro[1] = "             /     |      \\";
+		carro[2] = "        ------------------------------.";
+		carro[3] = "        |      Aprender kuya bwé      |";
+		carro[4] = "        |  ---                   ---  |";
+		carro[5] = "        --/( )\\-----------------/( )\\--";
+		
+
+		return carro;
+	}	
+}
